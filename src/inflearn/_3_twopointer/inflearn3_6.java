1cf9272ab919abe1f02c23e7d58cc87addd0db5c
LDG12 <99185099+LDG12@users.noreply.github.com>
2024-01-13 14:10:49 +0900

    Update inflearn3_6.java

--- src/inflearn/_3_twopointer/inflearn3_6.java
+++ src/inflearn/_3_twopointer/inflearn3_6.java
@@ -1,10 +1,40 @@
-package inflearn._3_twopointer;
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
 
-public class inflearn3_6 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Solution{
+	static int n,m,k;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int[] arr;
+	static ArrayList<Integer> list;
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int max=0;
+		int cnt=0;
+		int start=0;
+		for(int i=0; i<n; i++) {
+			if(arr[i]==0)cnt++;
+			if(cnt>m) {
+				while(arr[start]!=0) {
+					start++;
+				}
+				start++;
+				cnt--;
+			}
+			max = Math.max(max, i-start+1);
+		}
+		System.out.println(max);
+		br.close();
+		bw.close();
 	}
-
 }

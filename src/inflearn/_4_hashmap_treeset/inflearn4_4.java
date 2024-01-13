2c61440122003b168893a5f79387befa27b3ed92
LDG12 <99185099+LDG12@users.noreply.github.com>
2024-01-13 15:32:53 +0900

    Create inflearn4_4.java

---
+++ src/inflearn/_4_hashmap_treeset/inflearn4_4.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+ 
+class Solution{
+    static int n,m,k;
+    static StringTokenizer st;
+    static StringBuilder sb;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine();
+        String secondWord = br.readLine();
+        Map<Character, Integer>compareMap = new HashMap<>();
+        for(int i=0; i<secondWord.length(); i++) {
+        	char key = secondWord.charAt(i);
+        	compareMap.put(key, compareMap.getOrDefault(key, 0)+1);
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<secondWord.length()-1; i++) {
+        	char key = word.charAt(i);
+        	map.put(key, map.getOrDefault(key, 0)+1);
+        }
+        int cnt=0;
+        for(int i=secondWord.length()-1; i<word.length(); i++) {
+        	char key = word.charAt(i);
+        	map.put(key, map.getOrDefault(key, 0)+1);
+        	if(map.equals(compareMap))cnt++;
+        	key = word.charAt(i-secondWord.length()+1);
+        	map.put(key, map.getOrDefault(key, 1)-1);
+        	if(map.get(key)==0)map.remove(key);
+        }
+        System.out.println(cnt);
+        br.close();
+        bw.close();
+    }
+}
+

f471190835cfe59d991d679807683f21e6726aaf
LDG12 <person456@naver.com>
2024-01-23 20:25:20 +0900

    송아지 찾기 (BFS, 상태 트리 탐색)

--- src/inflearn/_7_dfs_bfs/inflearn7_5.java
+++ src/inflearn/_7_dfs_bfs/inflearn7_5.java
@@ -4,55 +4,57 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class inflearn7_5 {
 	static int n,m,k;
 	static int[] arr;
     static StringTokenizer st;
-    static StringBuilder sb; 
+    static StringBuilder sb;
+    static ArrayList<String> list;
+    static boolean[] visited;
+    static int min;
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n+1];
-        for(int i=1; i<=n; i++) {
-        	arr[i] = i;
-        }
-        sb = new StringBuilder();
-        dfs(1);
-        System.out.println(sb.toString());
-        sb = new StringBuilder();
-        dfs2(1);
-        System.out.println(sb.toString());
-        sb = new StringBuilder();
-        dfs3(1);
-        System.out.println(sb.toString());
+        st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        min = Integer.MAX_VALUE;
+        visited = new boolean[10001];
+        arr = new int[10001];
+        bfs(n);
+        System.out.println(min);
         br.close();
         bw.close();
     }
-    static void dfs(int start) {
-    	if(start>n)return;
-    	sb.append(arr[start]+" ");
-    	dfs(start*2);
-    	dfs(start*2+1);
-    }
-    static void dfs2(int start) {
-    	if(start>n) {
-    		return;
+    static void bfs(int start) {
+    	Queue<Integer> q = new LinkedList<>();
+    	q.offer(start);
+    	visited[start]=true;
+    	arr[start]=1;
+    	int l=0;
+    	int[] dx = {1,-1,5};
+    	while(!q.isEmpty()) {
+    		int len = q.size();
+    		for(int i=0; i<len; i++) {
+    			int now = q.poll();
+    			if(now==m) {
+    				min = l;
+    				break;
+    			}
+    			for(int j=0; j<3; j++) {
+    				int next = now+dx[j];
+    				if(next>=1 && next<=10000 && arr[next]==0) {
+    					q.offer(next);
+    					arr[next]=1;
+    				}
+    			}
+    		}
+    		l++;
     	}
-    	else {
-    		dfs2(start*2);
-    		sb.append(arr[start]+" ");
-        	dfs2(start*2+1);
-    	}
-    }
-    static void dfs3(int start) {
-    	if(start>n) {
-    		return;
-    	}
-    	dfs3(start*2);
-    	dfs3(start*2+1);
-    	sb.append(arr[start]+" ");
     }
 }

fa5d8bde255a179761f494ca722031edb169b24c
LDG12 <person456@naver.com>
2024-01-29 20:35:35 +0900

    6603 - 연산자 끼워넣기(2) ( 백트래킹, 재귀 )

--- src/baekjoon/Silver/baekjoon6603.java
+++ src/baekjoon/Silver/baekjoon6603.java
@@ -16,42 +16,51 @@ public class baekjoon6603 {
     static StringBuilder sb;
     static boolean[] visited;
     static Set<Integer> set;
+    static int max,min;
     static int[] arr2;
     static int head=1;
     static int tail=0;
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while(true) {
-        	st = new StringTokenizer(br.readLine(), " ");
-            n = Integer.parseInt(st.nextToken());
-            if(n==0)break;
-            arr = new int[n];
-            for(int i=0; i<n; i++) {
-            	arr[i] = Integer.parseInt(st.nextToken());
-            }
-            arr2 = new int[6];
-            visited = new boolean[n];
-            dfs(0,0);
-            System.out.println();
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[n];
+        for(int i=0; i<n; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
         }
+        st = new StringTokenizer(br.readLine(), " ");
+        arr2 = new int[4];
+        for(int i=0; i<4; i++) {
+        	arr2[i] = Integer.parseInt(st.nextToken());
+        }
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
+        dfs(1, arr[0]);
+        System.out.println(max);
+        System.out.println(min);
         br.close();
         bw.close();
     }
-    static void dfs(int depth, int start) {
-    	if(depth==6) {
-    		for(int i : arr2) {
-    			System.out.print(i+" ");
-    		}
-    		System.out.println();
+    static void dfs(int depth, int sum) {
+    	if(depth>=n) {
+    		max = Math.max(max, sum);
+    		min = Math.min(min, sum);
     		return;
     	}
-    	for(int i=start; i<n; i++) {
-    		if(visited[i] == false) {
-    			visited[i] = true;
-    			arr2[depth] = arr[i];
-    			dfs(depth+1, i);
-    			visited[i] = false;
+    	for(int i=0; i<4; i++) {
+    		if(arr2[i]!=0) {
+    			arr2[i]--;
+    			if(i==0) {
+    				dfs(depth+1, sum+arr[depth]);
+    			}else if(i==1) {
+    				dfs(depth+1, sum-arr[depth]);
+    			}else if(i==2) {
+    				dfs(depth+1, sum*arr[depth]);
+    			}else if(i==3) {
+    				dfs(depth+1, sum/arr[depth]);
+    			}
+    			arr2[i]++;
     		}
     	}
     }

dfebfba06b206ddb0f06617844c91db2d493d6d9
임동길 <person456@naver.com>
2024-02-05 16:24:21 +0900

    8-12 토마토(BFS)

--- src/inflearn/_8_dfs_bfs_uses/inflearn8_12.java
+++ src/inflearn/_8_dfs_bfs_uses/inflearn8_12.java
@@ -1,7 +1,9 @@
-package inflearn_8_dfs_bfs_uses;
+package inflearn._8_dfs_bfs_uses;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;

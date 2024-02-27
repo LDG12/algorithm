749af3094f6fe84a4ef9a98217560b339149b5dc
Limdonggil <person456@naver.com>
2024-02-27 11:38:01 +0900

    17070 - 파이프 옮기기 1 ( DP 사용 ) [G5]

--- src/baekjoon/Gold/baekjoon17070_DP.java
+++ src/baekjoon/Gold/baekjoon17070_DP.java
@@ -28,18 +28,18 @@ public class baekjoon17070_DP {
         }
         int[][][] dp = new int[n][n][3];
         dp[0][1][0] = 1;
-        for(int i=0; i<n; i++){
-            for(int j=2; j<n; j++){
-               if(arr[i][j] == 1)continue;
-               dp[i][j][0] = dp[i][j-1][0]+dp[i][j-1][2];
-               if(i==0)continue;
-               dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
-               if(arr[i-1][j] == 0 && arr[i][j-1]==0){
-                   dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1]+dp[i-1][j-1][2];
-               }
+        for (int i = 0; i < n; i++) {
+            for (int j = 2; j < n; j++) {
+                if (arr[i][j] == 1) continue;
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+                if (i == 0) continue;
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+                if (arr[i - 1][j] == 0 && arr[i][j - 1] == 0) {
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+                }
             }
         }
-        int result = dp[n-1][n-1][0]+dp[n-1][n-1][1]+dp[n-1][n-1][2];
+        int result = dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2];
         System.out.println(result);
         // 현재 상태가 0이면 가로, 1이면 세로, 2면 대각선
 

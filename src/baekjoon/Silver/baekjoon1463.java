4f3d1d73e5ec70fb315381862429017ee753b1ae
Limdonggil <person456@naver.com>
2024-02-27 11:38:17 +0900

    1463 - 1로 만들기 ( DP ) [S3]

--- src/baekjoon/Silver/baekjoon1463.java
+++ src/baekjoon/Silver/baekjoon1463.java
@@ -1,39 +1,16 @@
 package baekjoon.Silver;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 public class baekjoon1463 {
-	static int[] dx= {1,0,-1,0,1,-1,-1,1};
-	static int[] dy= {0,1,0,-1,1,-1,1,-1};
-	static int[] arr;
-	static int direction;
 	static int n,m,k;
-	static StringTokenizer st;
-	static boolean[] visited;
-	static int[][] result;
-	static int[] parent;
-	static int[] evenParent;
-	static ArrayList<Integer>trueMan; 
-	static ArrayList<Integer>[] party;
-	static int max;
-	static StringBuilder sb;
 	public static void main(String[] args) throws Exception {
-		//여기에 코드를 작성하세요.
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		int[] dp = new int[n+1];
-		dp[0] = dp[1] = 0;
-		for(int i=2; i<=n; i++) {
+		for(int i=2; i<=n; i++){
 			dp[i] = dp[i-1]+1;
-			if(i%2==0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1); 
-			}else if(i%3==0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1); 
-			}
+			if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+			if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
 		}
 		System.out.println(dp[n]);
 	}

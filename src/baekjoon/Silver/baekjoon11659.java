3be703f31fbe305e27ce4175e3bc81f57969ef3d
person456 <person456@naver.com>
2024-04-08 09:45:21 +0900

    11659 - 구간 합 구하기 4(세그먼트 트리 버전) ( 누적합 ) [S3]

--- src/baekjoon/Silver/baekjoon11659.java
+++ src/baekjoon/Silver/baekjoon11659.java
@@ -2,6 +2,7 @@ package baekjoon.Silver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -21,19 +22,38 @@ public class baekjoon11659 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
-		arr = new int[n+1];
 		m = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine()," ");
-		for(int i=1; i<=n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			arr[i] = arr[i-1]+num; 
+		int depth = 0;
+		while(Math.pow(2,depth)<n){
+			depth++;
 		}
-		for(int i=0; i<m; i++) {
-			int sum=0;
-			st = new StringTokenizer(br.readLine()," ");
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			System.out.println(arr[end]-arr[start-1]);
+		depth++;
+		int treeSize = (int)Math.pow(2, depth);
+		int startIndex = (int)Math.pow(2, depth-1);
+		int[] segmentTree = new int[treeSize];
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<n; i++){
+			segmentTree[startIndex+i] = Integer.parseInt(st.nextToken());
 		}
+		for(int i = startIndex-1; i>=1; i--){
+			segmentTree[i] = segmentTree[i*2] + segmentTree[i*2+1];
+		}
+		sb = new StringBuilder();
+		for(int i=0; i<m; i++){
+			st = new StringTokenizer(br.readLine(),  " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int start = a+startIndex-1;
+			int end = b+startIndex-1;
+			int ans = 0;
+			while(start<=end){
+				if(start%2==1)ans+=segmentTree[start];
+				if(end%2==0)ans+=segmentTree[end];
+				start = (start+1)/2;
+				end = (end-1)/2;
+			}
+			sb.append(ans+"\n");
+		}
+		System.out.print(sb.toString());
 	}
 }
